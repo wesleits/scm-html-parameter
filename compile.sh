bd29accf5729d574a3eeff17c2096dd5765807b4
Weslei Teixeira da Silveira <wesleiteixeira@hotmail.com.br>
2017-12-13 00:41:13 +0000

    scripts updated

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 ##### Script works correctly in Ubuntu 14.04/16.04                                    #####
 ##### To install this plugin execute in jenkins machine these commands:               #####
 #####                                                                                 #####
-##### wget -N https://goo.gl/BaC93u -P /var/lib/jenkins/plugins --content-disposition #####
+##### wget -N https://goo.gl/rLBxv6 -P /var/lib/jenkins/plugins --content-disposition #####
 ##### service jenkins restart                                                         ##### 
 ###########################################################################################
 
